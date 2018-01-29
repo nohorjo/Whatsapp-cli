b5e13b958a5f6c7c6b8a559d7195276921212f03
Muhammed Haque <muhammed.haque@bond.co.uk>
2018-01-29 08:55:38 +0000

    Fixed duplicate last message

--- index.ts
+++ index.ts
@@ -80,43 +80,45 @@ const printQRcode = async page => {
 
         const text = async elem => await (await elem.getProperty('textContent')).jsonValue();
 
-        const printPeople = () => people.forEach(async (person, i) => {
-            console.log(`${i + 1} - ${await (await person.getProperty('textContent')).jsonValue()}`);
-        });
-
         let messageScanner;
 
-        await printPeople();
-        rl.question("Who would you like to chat to?\n> ", async (answer) => {
-            const printMessage = async msg => {
-                const inMsg = await msg.$(SEL_IN_MESSAGE);
-                const outMsg = await msg.$(SEL_OUT_MESSAGE);
-                const msgText = await (await (inMsg || outMsg).getProperty('textContent')).jsonValue();
-                console.log(colors[outMsg ? "white" : "green"](msgText));
-
-            };
-            people[parseInt(answer) - 1].click();
-            await page.waitForSelector(SEL_MSG, { timeout: 60000 });
-            const msgs = (await page.$$(SEL_MSG)).slice(-10);
-            msgs.forEach(printMessage);
-            messageScanner = (() => {
-                let lastTime;
-                return setInterval(async () => {
-                    const time = await (await (await page.$$(SEL_TIME)).slice(-1)[0].getProperty('textContent')).jsonValue();
-                    if (time != lastTime) {
-                        lastTime = time;
-                        printMessage((await page.$$(SEL_MSG)).slice(-1)[0]);
-                    }
-                }, 200);
-            })();
-            rl.on("line", async line => {
-                await (await page.$(SEL_MSG_INPUT)).type(line);
-                (await page.$(SEL_BUTTON_SEND)).click();
+        const printPeople = () => {
+            people.forEach(async (person, i) => {
+                console.log(`${i + 1} - ${await (await person.getProperty('textContent')).jsonValue()}`);
             });
-        });
+            rl.question("Who would you like to chat to?\n> ", async (answer) => {
+                const printMessage = async msg => {
+                    const inMsg = await msg.$(SEL_IN_MESSAGE);
+                    const outMsg = await msg.$(SEL_OUT_MESSAGE);
+                    const msgText = await (await (inMsg || outMsg).getProperty('textContent')).jsonValue();
+                    console.log(colors[outMsg ? "white" : "green"](msgText));
+
+                };
+                people[parseInt(answer) - 1].click();
+                await page.waitForSelector(SEL_MSG, { timeout: 60000 });
+                const msgs = (await page.$$(SEL_MSG)).slice(-10);
+                msgs.forEach(printMessage);
+                messageScanner = (() => {
+                    let lastTime;
+                    return setInterval(async () => {
+                        const time = await (await (await page.$$(SEL_TIME)).slice(-1)[0].getProperty('textContent')).jsonValue();
+                        if (lastTime && lastTime != time) {
+                            printMessage((await page.$$(SEL_MSG)).slice(-1)[0]);
+                        }
+                        lastTime = time;
+                    }, 200);
+                })();
+                rl.on("line", async line => {
+                    await (await page.$(SEL_MSG_INPUT)).type(line);
+                    (await page.$(SEL_BUTTON_SEND)).click();
+                });
+            });
+        };
+
+        await printPeople();
 
     } catch (e) {
-        console.error(e);
+        console.trace(e);
         cleanUpAndQuit(browser)();
     }
 })();
